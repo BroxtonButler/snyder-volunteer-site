6b3a9e675c62781e25a15447d9f91e0945025365
BroxtonButler <broxtonbutler@outlook.com>
2024-05-09 13:54:39 -0400

    Added owen's stuff and made changes where needed

---
+++ Data/DataBase/ISqlDataAccess.cs
@@ -0,0 +1,10 @@
+﻿namespace snyder_volunteer_site.Data.DataBase
+{
+	public interface ISqlDataAccess
+	{
+		Task<List<T>> LoadData<T>(string storedProc, string connectionName, object? parameters);
+
+		//this is the interface
+		Task SaveData(string storedProc, string connectionName, object parameters);
+	}
+}
---
+++ Data/DataBase/SqlDataAccess.cs
@@ -0,0 +1,43 @@
+﻿using Dapper;
+using Microsoft.Data.SqlClient;
+
+namespace snyder_volunteer_site.Data.DataBase
+{
+	public class SqlDataAccess : ISqlDataAccess
+	{
+		private readonly IConfiguration _config;
+
+		public SqlDataAccess(IConfiguration config)
+		{
+			_config = config;
+		}
+
+		public async Task<List<T>> LoadData<T>(
+				string storedProc,
+				string connectionName,
+				object? parameters)
+		{
+			string connectionString = _config.GetConnectionString(connectionName)
+					?? throw new Exception($"Missing connection string at {connectionName}");
+
+			using var connection = DBConnection.Connection;
+
+			var rows = await connection.QueryAsync<T>(storedProc, parameters, commandType: System.Data.CommandType.StoredProcedure);
+
+			return rows.ToList();
+		}
+
+		public async Task SaveData(
+				string storedProc,
+				string connectionName,
+				object parameters)
+		{
+			string connectionString = _config.GetConnectionString(connectionName)
+					?? throw new Exception($"Missing connection string at {connectionName}");
+
+			using var connection = new SqlConnection(connectionString);
+
+			await connection.ExecuteAsync(storedProc, parameters, commandType: System.Data.CommandType.StoredProcedure);
+		}
+	}
+}
